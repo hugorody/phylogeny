5f9ee214a7900740f2457186070e3710aef175f3
Hugo Rody <hugo.rody@unifesp.br>
2018-03-28 09:58:24 -0300

    Update fasta2NJnewick.R

--- fasta2NJnewick.R
+++ fasta2NJnewick.R
@@ -1,13 +1,13 @@
 library(phangorn)
 library(ape)
 
-myfiles <- list.files(path = "/home/hugo/Dropbox/Unifesp/Trabalhos/Genes_duplicados/3018/", pattern = NULL, all.files = FALSE,
+myfiles <- list.files(path = "/path/to/fasta/msa/files/", pattern = NULL, all.files = FALSE,
            full.names = FALSE, recursive = FALSE,
            ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
            
 for (fastafile in myfiles) {
 
-infile <- paste(c("/home/hugo/Dropbox/Unifesp/Trabalhos/Genes_duplicados/3018/",fastafile),collapse="")
+infile <- paste(c("/path/to/fasta/msa/files/",fastafile),collapse="")
 outfile <- paste(c(fastafile,".nwk"),collapse="")
 
 print (paste(infile))
